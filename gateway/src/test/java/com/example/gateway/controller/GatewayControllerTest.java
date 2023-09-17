325f0a0f8b8ab6b361df77252428ba7db1306338
Tunebaker <tunebaker@gmail.com>
2023-09-17 23:02:18 +0300

    tests added

--- gateway/src/test/java/com/example/gateway/controller/GatewayControllerTest.java
+++ gateway/src/test/java/com/example/gateway/controller/GatewayControllerTest.java
@@ -1,11 +1,12 @@
-package com.example.gateway.controller.impl;
+package com.example.gateway.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -28,21 +30,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class GatewayControllerTest {
+public class GatewayControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeAll
-    static void init() {
-        WireMockServer wireMockServer = new WireMockServer(
+    WireMockServer wireMockServer;
+
+    @BeforeEach
+    void init() {
+        wireMockServer = new WireMockServer(
                 new WireMockConfiguration().port(8082));
         wireMockServer.start();
         WireMock.configureFor("localhost", 8082);
     }
 
+    @AfterEach
+    void close() {
+        wireMockServer.stop();
+    }
+
     @Test
-    @DisplayName("Контроллер должен возвращать статус ОК при валидном аргументе")
+    @DisplayName("createLoanApplication должен возвращать статус ОК и правильный ответ при валидном аргументе ")
     void createLoanApplication() throws Exception {
         String createApplicationRequest = String.join("", Files.readAllLines(Path.of("src/test/resources/create-application-request.json")));
         String createApplicationResponse = String.join("", Files.readAllLines(Path.of("src/test/resources/create-application-response.json")));
@@ -65,14 +74,14 @@ class GatewayControllerTest {
     }
 
     @Test
-    @DisplayName("Контроллер должен возвращать статус 500 при ошибке сервера")
+    @DisplayName("createLoanApplication должен возвращать статус 500 при ошибке сервера")
     void createLoanApplicationWithServerError() throws Exception {
         String createApplicationRequest = String.join("", Files.readAllLines(Path.of("src/test/resources/create-application-request.json")));
 
         stubFor(WireMock.post(urlMatching("/application"))
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.INTERNAL_SERVER_ERROR.value())
-        ));
+                ));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/application")
                         .content(createApplicationRequest)
@@ -83,14 +92,9 @@ class GatewayControllerTest {
     }
 
     @Test
-    @DisplayName("Контроллер должен возвращать статус 400 при невалидном запросе")
+    @DisplayName("createLoanApplication должен возвращать статус 400 при невалидном запросе")
     void createLoanApplicationWithBadRequestParams() throws Exception {
         String createApplicationBadRequest = String.join("", Files.readAllLines(Path.of("src/test/resources/create-application-bad-request.json")));
-
-        stubFor(WireMock.post(urlMatching("/application"))
-                .willReturn(aResponse()
-                        .withStatus(HttpStatus.BAD_REQUEST.value())
-                ));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/application")
                         .content(createApplicationBadRequest)
@@ -100,5 +104,40 @@ class GatewayControllerTest {
         ;
     }
 
+
+    @Test
+    @DisplayName("chooseOneOffer должен возвращать статус ОК при валидном аргументе")
+    void chooseOneOffer() throws Exception {
+        String chooseOfferRequest = String.join("",
+                Files.readAllLines(Path.of("src/test/resources/apply-offer-request.json")));
+
+        stubFor(WireMock.put(urlMatching("/application/offer"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", "application/json")
+                ));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/application/apply")
+                        .content(chooseOfferRequest)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()
+                );
+    }
+
+
+    @Test
+    @DisplayName("chooseOneOffer должен возвращать статус 400 при невалидном запросе")
+    void chooseOneOfferWithBadArgs() throws Exception {
+        String chooseOfferRequest = String.join("",
+                Files.readAllLines(Path.of("src/test/resources/apply-offer-bad-request.json")));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/application/apply")
+                        .content(chooseOfferRequest)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest()
+                );
+    }
 
 }
