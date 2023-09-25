14d84626ee2f4e6461991f1e1728f776cb0ffd64
Tunebaker <123461586+Tunebaker@users.noreply.github.com>
2023-09-25 15:17:52 +0300

    fix import DocumentServiceImpl.java

--- deal/src/main/java/com/example/deal/service/impl/DocumentServiceImpl.java
+++ deal/src/main/java/com/example/deal/service/impl/DocumentServiceImpl.java
@@ -17,7 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 import static com.example.deal.model.ApplicationStatusHistoryDTO.StatusEnum.CREDIT_ISSUED;
 import static com.example.deal.model.ApplicationStatusHistoryDTO.StatusEnum.DOCUMENT_SIGNED;
